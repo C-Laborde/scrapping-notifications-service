a4cd4dc21afd0fe1a9232fb6fcd8418e1d886007
cecilia_laborde <cecilialaborde@gmail.com>
2020-03-13 17:35:31 +0100

    Games fully scrapped

--- main_scrapping.py
+++ main_scrapping.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.4.0
+#       jupytext_version: 1.3.3
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -35,28 +35,70 @@ else:
 
 soup1 = BeautifulSoup(page.content, 'html.parser')
 
-result = soup1.select('div .resultados')
+# Ideally there're 4 div. resultados (tables)
+resultados = soup1.select('div .resultados')
+# TODO check if len of results is 4
+
+useful_results = resultados[1].select('tr')
 # TODO use columns names to check if right table
+print(useful_results[0])
+# TODO check if len(useful_results is 5, the table has 5 rows)
+
+# +
+games_results = []
+table_rows = []
 games = []
-for i in range(1, len(result[1].select('tr'))):
-    games.append(result[1].select('tr')[i])
+# The first row are the columns labels, games details start on the second row
+for i in range(1, len(useful_results)):
+    table_rows.append(useful_results[i])
 
-list(games[0].find_all('td'))
+for game in table_rows:
+    game_components = game.find_all('td')
+    res = game_components[1].contents
+    game_struct = {"LOCAL": game_components[0].find('a', class_='discreto').contents,
+                   "VISITANT": game_components[2].find('a', class_='discreto').contents,
+                   "RESUL": res,
+                   "SETS": game_components[3].contents}
+    games_results.append(res)
+    games.append(game_struct)
 
-games[0].find_all('td')[1].contents
+# TODO check that all values in dics have length 1
+# -
+
+games
 
 url2 = "http://competicio.fcvoleibol.cat/competiciones.asp?torneo=4253&jornada=7"
 page = requests.get(url2)
 soup2 = BeautifulSoup(page.content, 'html.parser')
 
+# Ideally there're 4 div. resultados (tables)
+resultados = soup2.select('div .resultados')
+# TODO check if len of results is 4
 
-result2 = soup2.select('div .resultados')
+useful_results = resultados[1].select('tr')
+# TODO use columns names to check if right table
+print(useful_results[0])
+# TODO check if len(useful_results is 5, the table has 5 rows)
+
+# +
+games_results2 = []
+table_rows = []
 games2 = []
-for i in range(1, len(result2[1].select('tr'))):
-    games2.append(result2[1].select('tr')[i])
+# The first row are the columns labels, games details start on the second row
+for i in range(1, len(useful_results)):
+    table_rows.append(useful_results[i])
 
-list(games2[0].find_all('td'))
+for game in table_rows:
+    game_components = game.find_all('td')
+    res = game_components[1].contents
+    game_struct = {"LOCAL": game_components[0].find('a', class_='discreto').contents,
+                   "VISITANT": game_components[2].find('a', class_='discreto').contents,
+                   "RESUL": res,
+                   "SETS": game_components[3].contents}
+    games_results2.append(res)
+    games2.append(game_struct)
 
-list(games2[0].find_all('td'))[1].contentsy
+# TODO check that all values in dics have length 1
+# -
 
-
+games2
