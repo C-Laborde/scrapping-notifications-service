b5edf49dfcec85583ab76c7e2cd32adf264db197
cecilia_laborde <cecilialaborde@gmail.com>
2020-03-15 21:40:13 +0100

    Added specs to continue

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from google.cloud import firestore
+
+db = firestore.Client()
+
+def parse_res(res):
+    res = res.replace(" ", "")
+    try:
+        res_loc = res.split("-")[0]
+        res_vis = res.split("-")[1]
+    except IndexError:
+        res_loc = "None"
+        res_vis = "None"
+    return res_loc, res_vis
+
+def parse_sets(sets):
+    try:
+        return sets[0]
+    except IndexError:
+        return "None"
+    
 
 
 def main(request):
@@ -14,35 +34,37 @@ def main(request):
         print("Page status code: %s Error downloading" % page.status_code)
 
 
+    #### Next section is about getting the results new or not
     soup1 = BeautifulSoup(page.content, 'html.parser')
 
     # Ideally there're 4 div. resultados (tables)
-    resultados = soup1.select('div .resultados')
+    all_tables = soup1.select('div .resultados')
     # TODO check if len of results is 4
 
-    useful_results = resultados[1].select('tr')
+    table = all_tables[1].select('tr')
     # TODO use columns names to check if right table
-    print(useful_results[0])
     # TODO check if len(useful_results is 5, the table has 5 rows)
 
-    # +
-    games_results = []
     table_rows = []
     games = []
     # The first row are the columns labels, games details start on the second row
-    for i in range(1, len(useful_results)):
-        table_rows.append(useful_results[i])
-
-    for game in table_rows:
+    for i in range(1, len(table)):
+        game = table[i]
         game_components = game.find_all('td')
         res = game_components[1].contents
+        res_loc, res_vis = parse_res(res[0])
+        sets = parse_sets(game_components[3].contents)
         game_struct = {"LOCAL": game_components[0].find('a', class_='discreto').contents,
-                       "VISITANT": game_components[2].find('a', class_='discreto').contents,
-                       "RESUL": res,
-                       "SETS": game_components[3].contents}
-        games_results.append(res)
+                    "VISITANT": game_components[2].find('a', class_='discreto').contents,
+                    "RESULT-LOCAL": res_loc,
+                    "RESULT-VISITANT": res_vis,
+                    "SETS": sets}
         games.append(game_struct)
     
+    # If all the results are empty we can finish here. Only dump when there
+    # has been a result
+    
+    # This is to format the document into a json format
     weekend_id = "WEEKEND" + WEEKEND
     names_parse = {"CEV L‘HOSPITALET 'B'": "CEV LHOSPITALET B",
                    "CLUB VÒLEI LA PALMA": "CLUB VOLEY LA PALMA",
@@ -53,10 +75,19 @@ def main(request):
                    "IGUALADA VÒLEI CLUB": "IGUALADA VOLEI CLUB",
                    "DSV CV SANT CUGAT 'D'": "DSV CV SANT CUGAT D"}
 
-    document = {weekend_id: {}}
+    document = {}
     for i, game in enumerate(games):
-        document[weekend_id]["GAME" + str(i + 1)] = {k: names_parse.get(v[0], v[0]) for k, v in game.items()}
-   
-    print("DOCUMENT: ", document)
+        document["GAME" + str(i + 1)] = {k: names_parse.get(v[0], v[0]) for k, v in game.items()}
     # TODO check that all values in dics have length 1
-    return document
+
+    # Now we should check for the same doc in the database.
+    # It it doesn't exist: dump the doc
+    doc_ref = db.collection(u'games').document(weekend_id)
+    doc_ref.set(document)
+
+    # It it exists: load it and compare
+    doc = db.collection(u'games').document(weekend_id).get()
+    
+    # return doc.to_dict()
+    print("DOC: ", doc.to_dict())
+    return str(200)
