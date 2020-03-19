56e2addec4671053251be9969ee81db803d9d56d
cecilia_laborde <cecilialaborde@gmail.com>
2020-03-19 10:47:45 +0100

    Sending email template working

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 from google.cloud import firestore
+import json
+import smtplib
+from email.mime.text import MIMEText
+
 
 db = firestore.Client()
 
@@ -22,7 +26,6 @@ def parse_sets(sets):
     except IndexError:
         return "-"
 
-# TODO
 def doc_comparison(restored, document):
     if not sorted(restored.keys()) == sorted(document.keys()):
         print("RESTORED KEYS: ", restored.keys())
@@ -36,6 +39,23 @@ def doc_comparison(restored, document):
             return False
     return True
 
+
+def send_email():
+    print("HERE!")
+    with open("mail_cred.json", "r") as f:
+        cred = json.load(f)
+    msg = MIMEText('Testing some Mailgun awesomness')
+    msg['Subject'] = "Hello"
+    msg['From']    = cred["mailgun"]["username"]
+    msg['To']      = cred["mailgun"]["to"]
+
+    s = smtplib.SMTP('smtp.mailgun.org', 587)
+
+    # s.login('postmaster@YOUR_DOMAIN_NAME', '3kh9umujora5')
+    s.login(cred["mailgun"]["username"], cred["mailgun"]["password"])
+    s.sendmail(msg['From'], msg['To'], msg.as_string())
+    s.quit()
+    
 
 def main(request):
     WEEKEND = str(6)
@@ -98,14 +118,16 @@ def main(request):
         document["GAME" + str(i + 1)] = {k: names_parse.get(v[0], v[0])
                                          for k, v in game.items()}
     # TODO check that all values in dics have length 1
-
+    send_email()
     # Now we should check for the same doc in the database.
     # If it doesn't exist: dump the doc
     doc_ref = db.collection(u'games').document(weekend_id)
     restored_doc = doc_ref.get()
     if not restored_doc.exists:
         doc_ref.set(document)
-        # TODO send email!
+        # send_email(document.to_dict())
+        print("BEFORE HERE")
+        # send_email()
     # It it exists: load it and compare
     else: 
         # TODO compare restored_doc with document. Be carefull with N and None
