ac8ac7344efc54c7fe5c73c025aab48ea45973de
cecilia_laborde <cecilialaborde@gmail.com>
2020-03-29 17:41:43 +0200

    Better html email format

--- main.py
+++ main.py
@@ -41,9 +41,56 @@ def doc_comparison(restored, document):
             return False
     return True
 
+def html_msg(weekend_nr, document):
+    return f"""\
+    <html>
+        <body>
+            <p>
+            Hola, <br> Hay nuevos resultados disponibles de la <//br>
+            <a href= "http://competicio.fcvoleibol.cat/competiciones.asp?torneo=4253&jornada=6"> jornada {weekend_nr} </a>
+            </p>
+            <div class="resultados">
+                <h3> RESULTATS D'AQUESTA JORNADA </h3>
+                <table width="800" border="0" align="center" cellpadding="2" cellspacing="2" class="tabla">
+                    <tbody>
+                        <tr class="tittr">
+                            <td width="30%" height="13" valign="top" bgcolor="#A9F5E1"> LOCAL </td>
+                            <td width="4%" valign="top" bgcolor="#A9F5E1"> RESUL </td>
+                            <td width="30%" valign="top" bgcolor="#A9F5E1"> VISITANT </td>
+                            <td width="36%" valign="top" bgcolor="#A9F5E1"> SETS </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#81BEF7"> {document["GAME1"]["LOCAL"]} </td>
+                            <td bgcolor="#81BEF7" align="center"> {document["GAME1"]["RESULT-LOCAL"]} - {document["GAME1"]["RESULT-VISITANT"]} </td>
+                            <td bgcolor="#81BEF7"> {document["GAME1"]["VISITANT"]} </td>
+                            <td bgcolor="#81BEF7"> {document["GAME1"]["SETS"]} </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#81DAF5"> {document["GAME2"]["LOCAL"]} </td>
+                            <td bgcolor="#81DAF5" align="center"> {document["GAME2"]["RESULT-LOCAL"]} - {document["GAME2"]["RESULT-VISITANT"]} </td>
+                            <td bgcolor="#81DAF5"> {document["GAME2"]["VISITANT"]} </td>
+                            <td bgcolor="#81DAF5"> {document["GAME2"]["SETS"]} </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#81BEF7"> {document["GAME3"]["LOCAL"]} </td>
+                            <td bgcolor="#81BEF7" align="center"> {document["GAME3"]["RESULT-LOCAL"]} - {document["GAME3"]["RESULT-VISITANT"]} </td>
+                            <td bgcolor="#81BEF7"> {document["GAME3"]["VISITANT"]} </td>
+                            <td bgcolor="#81BEF7"> {document["GAME3"]["SETS"]} </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#81DAF5"> {document["GAME4"]["LOCAL"]} </td>
+                            <td bgcolor="#81DAF5" align="center"> {document["GAME4"]["RESULT-LOCAL"]} - {document["GAME4"]["RESULT-VISITANT"]} </td>
+                            <td bgcolor="#81DAF5"> {document["GAME4"]["VISITANT"]} </td>
+                            <td bgcolor="#81DAF5"> {document["GAME4"]["SETS"]} </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>   
+        </body>
+    </html>
+    """
 
 def send_email(weekend_id, document):
-    print("HERE!")
     smtp_username = os.getenv("SMTP_USERNAME")
     smtp_password = os.getenv("SMTP_PASSWORD")
     smtp_port = os.getenv("SMTP_PORT")
@@ -51,35 +98,25 @@ def send_email(weekend_id, document):
     smtp_to = os.getenv("SMTP_TO")
 
     weekend_nr = weekend_id.split("WEEKEND")[1]
+    # TODO add text email
     text = '\
         Hay nuevos resultados disponibles de la jornada ' + weekend_nr
-    html = f"""\
-    <html>
-        <body>
-            <p>
-            Hola, <br> Hay nuevos resultados disponibles de la <//br>
-            <a href= "http://competicio.fcvoleibol.cat/competiciones.asp?torneo=4253&jornada=6"> jornada {weekend_nr} </a>
-            </p>
-            <p>
-            {document}
-            </p>
-        </body>
-    </html>
-    """
-    msg = MIMEText(text)
-    # msg = MIMEMultipart("alternative")
+    html = html_msg(weekend_nr, document)
+
+    # msg = MIMEText(text)
+    msg = MIMEMultipart("alternative")
     msg['Subject'] = "FCVResultats jornada " + weekend_id
     msg['From']    = smtp_username
     msg['To']      = smtp_to
 
     # Turn these into plain/html MIMEText objects
     part1 = MIMEText(text, "plain")
-    # part2 = MIMEText(html, "html")
+    part2 = MIMEText(html, "html")
 
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
-    # msg.attach(part1)
-    # msg.attach(part2)
+    msg.attach(part1)
+    msg.attach(part2)
 
     # Create a secure SSL context
     context = ssl.create_default_context()
@@ -156,7 +193,7 @@ def main(request):
         document["GAME" + str(i + 1)] = {k: names_parse.get(v[0], v[0])
                                          for k, v in game.items()}
     # TODO check that all values in dics have length 1
-
+    send_email(weekend_id, document)
     # Now we should check for the same doc in the database.
     # If it doesn't exist: dump the doc
     doc_ref = db.collection(u'games').document(weekend_id)
